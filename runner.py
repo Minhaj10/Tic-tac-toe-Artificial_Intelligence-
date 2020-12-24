b6e604c37963d675c05e9139b34e943a8854a002
Admin <minhajumid987@gmail.com>
2020-12-24 20:31:36 +0600

    runner funtion initialize

---
+++ runner.py
@@ -0,0 +1,22 @@
+import pygame
+import sys
+import time
+
+import tictactoe as ttt
+
+pygame.init()
+size = width, height = 600, 400
+
+# Colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+screen = pygame.display.set_mode(size)
+
+mediumFont = pygame.font.Font("OpenSans-Regular.ttf", 28)
+largeFont = pygame.font.Font("OpenSans-Regular.ttf", 40)
+moveFont = pygame.font.Font("OpenSans-Regular.ttf", 60)
+
+user = None
+board = ttt.initial_state()
+ai_turn = False
